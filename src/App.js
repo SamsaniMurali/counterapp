6601ed3e11a3eb724f51cd421c8b4fcaacdd44cc
unknown <samsanimurali1999@gmail.com>
2023-02-23 11:47:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Style.css'
-
-const App = () => {
+function App() {
+  const[counter,setCounter]=useState(0)
+  const stock=10;
   return (
-    <div className='flex'>
- 
-        <h1> Background Color Changer</h1>
-        <input type='color' onChange={(e)=>{
-            document.body.style.background=e.target.value
-        }}/>
-    
+    <div className='wrapper'>
+    <h1>Counter App</h1>
+      <button className='minus' disabled={counter===0} onClick={()=>{
+        if(counter>0){
+          setCounter(counter-1)
+        }
+      }}>-</button>
+      <p className='counter'>{counter}</p>
+      <button className='plus' disabled={counter===stock} onClick={()=>{
+        if(counter<stock){
+          setCounter(counter+1)
+        }
+      }}>+</button>
     </div>
   )
 }
